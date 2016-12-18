2a53115a7270317b0fbb0dc4fb5d957b31eaa0fe
Blake Hagen <blakehagen@gmail.com>
2016-12-18 13:47:02 -0700

    heaviestFilms

--- server/controllers/films.controller.js
+++ server/controllers/films.controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const _        = require('lodash');
+const numeral  = require('numeral');
 const BPromise = require('bluebird');
 const brequest = BPromise.promisify(require('request'));
 
@@ -8,9 +9,41 @@ module.exports = {
 
   getHeaviestFilms: (req, res) => {
 
+    let filmOptions = {
+      method: 'GET',
+      uri: 'http://swapi.co/api/films/',
+    };
 
+    brequest(filmOptions)
+      .then(response => {
+        let filmData = _.attempt(JSON.parse, response.body).results;
 
+        BPromise.map(filmData, film => {
+          return BPromise.map(film.characters, character => {
+            return brequest({method: 'GET', uri: character})
+              .then(response => {
+                let characterMassString = _.attempt(JSON.parse, response.body).mass;
+                let characterMass       = numeral(characterMassString)._value;
+                return characterMass;
+              })
+          })
+            .then(response => {
+              film.characterMass = response;
+              return film;
+            })
+        })
+          .then(films => {
+
+            _.each(films, film => {
+              film.characterMass = _.sum(_.compact(film.characterMass));
+            });
+
+            return res.status(200).json(_.reverse(_.sortBy(films, 'characterMass')));
+          })
+      })
+      .catch(err => {
+        return res.status(500).json(err);
+      })
   }
-
 
 };
